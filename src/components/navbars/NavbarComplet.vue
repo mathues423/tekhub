418344c40bfe4e4f1b3b638ed4d8464c09fbec75
Matheus dos Reis Casarim <matheuscasarim22@hotmail.com>
2024-10-20 17:00:37 -0300

    Adição de icones: mudar para componentes se viavel

--- src/components/navbars/NavbarComplet.vue
+++ src/components/navbars/NavbarComplet.vue
@@ -1,13 +1,74 @@
 <script lang="ts">
       import NavbarItem from "./NavbarItem.vue";
       import router from '@/router';
-      const dashboard = {icon : '', abrev: 'Dashboard', rota: '/dashboard', activi: false};
-      const empresas = {icon : '', abrev: 'Empresas', rota: '/empresas', activi: false};
-      const canais = {icon : '', abrev: 'Canais', rota: '/canais', activi: false};
-      const ambiente = {icon : '', abrev: 'Ambiente', rota: '/ambiente', activi: false};
-      const usuario = {icon : '', abrev: 'Usuário', rota: '/usuario', activi: false};
-      const logatt = {icon : '', abrev: 'Logs das Atualizações', rota: '/logatt', activi: false};
-      const logreq = {icon : '', abrev: 'Logs das Requisições', rota: '/logreq', activi: false};
+      //Icones
+      const icon_wid = '16', icon_hgt = '16';
+      //dashboard
+      const dashboard = {icon : {
+            width: icon_wid,
+            height: icon_hgt,
+            class : 'bi bi-speedometer',
+            path : '<path d="M8 2a.5.5 0 0 1 .5.5V4a.5.5 0 0 1-1 0V2.5A.5.5 0 0 1 8 2M3.732 3.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707M2 8a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8m9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5m.754-4.246a.39.39 0 0 0-.527-.02L7.547 7.31A.91.91 0 1 0 8.85 8.569l3.434-4.297a.39.39 0 0 0-.029-.518z"/> <path fill-rule="evenodd" d="M6.664 15.889A8 8 0 1 1 9.336.11a8 8 0 0 1-2.672 15.78zm-4.665-4.283A11.95 11.95 0 0 1 8 10c2.186 0 4.236.585 6.001 1.606a7 7 0 1 0-12.002 0"/>'
+      } , 
+      abrev: 'Dashboard', rota: '/dashboard', act: false};
+      //empresas
+      const empresas = {icon : {
+            width: icon_wid,
+            height: icon_hgt,
+            class : 'bi bi-shop',
+            path : '  <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.37 2.37 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0M1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5M4 15h3v-5H4zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1zm3 0h-2v3h2z"/>'
+            
+      }, abrev: 'Empresas', rota: '/empresas', act: false};
+      //canais
+      const canais = {icon : {
+            width: icon_wid,
+            height: icon_hgt,
+            class : 'bi bi-grid-fill',
+            path : '<path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5z"/>'
+            
+      }, abrev: 'Canais', rota: '/canais', act: false};
+      //ambiente
+      const ambiente = {icon : {
+            width: icon_wid,
+            height: icon_hgt,
+            class : 'bi bi-cloud-fill',
+            path : '<path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383"/>'
+            
+      }, abrev: 'Ambiente', rota: '/ambiente', act: false};
+      //integrações
+      const integracao= {icon : {
+            width: icon_wid,
+            height: icon_hgt,
+            class : 'bi bi-arrow-repeat',
+            path : '<path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9"/><path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"/>'
+            
+      }, abrev: 'Integração', rota: undefined, act: false};
+            //Marketplace/Ecommerce
+            //Mapeamento de Produtos
+      //usuario
+      const usuario = {icon : {
+            width: icon_wid,
+            height: icon_hgt,
+            class : 'bi bi-person-fill',
+            path : '<path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>'
+            
+      }, abrev: 'Usuário', rota: '/usuario', act: false};
+      //Log de att
+      const logatt = {icon : {
+            width: icon_wid,
+            height: icon_hgt,
+            class : 'bi bi-journal-text',
+            path : '<path d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5"/><path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2"/><path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z"/>'
+            
+      }, abrev: 'Logs das Atualizações', rota: '/logatt', act: false};
+      //Log das reqs
+      const logreq = {icon : {
+            width: icon_wid,
+            height: icon_hgt,
+            class : 'bi bi-card-list',
+            path : ' <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/><path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8m0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0M4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0"/>'
+           
+      }, abrev: 'Logs das Requisições', rota: '/logreq', act: false};
       export default {
             data : () => ({
                   conteudo : [dashboard, empresas, canais, ambiente, usuario, logatt, logreq]
@@ -18,6 +79,7 @@
             },
 
             methods:{
+                  // Limpar cache
                   logout(){
                         router.push('/');
                   }
@@ -45,7 +107,8 @@
       </div>
       <div class="nav_lateral col-2" style="height: 300px">
             <ul class="itens_nav" v-for="navitem in conteudo" v-bind:key="navitem.abrev">
-                  <NavbarItem :nav_item="navitem"/>
+                  <NavbarItem v-if="navitem.act" class="itens_nav_act" :nav_item="navitem"/>
+                  <NavbarItem v-else :nav_item="navitem"/>
             </ul>
       </div>
 </template>
@@ -57,23 +120,23 @@
 
 .nav_lateral{
       background-color: var(--bs-primary-text-emphasis);
+      padding: 0;
 }
 
 .itens_nav{
-      padding-left: 24px;
-      font-size: 14px;
-      color: var(--bs-grey-100);
+      padding-left: 0;
+      margin-bottom: 0;
 }
 
-.itens_nav:focus{
-      background-color: var(--bs-cyan);
-      color: var(--bs-gray-700);
-}
 .itens_nav:active{
-      background-color: var(--bs-primary-bg-subtle);
-      color: var(--bs-gray-700);
+      background-color: var(--bs-cyan);
+      color: var(--bs-gray-200);
 }
 
+.itens_nav_act{
+      background-color: var(--bs-cyan);
+      color: var(--bs-gray-200);
+}
 /* Media dark?
 @media (prefers-color-scheme: dark) {
 
--- src/components/navbars/NavbarItem.vue
+++ src/components/navbars/NavbarItem.vue
@@ -2,20 +2,32 @@
 
 export default{
       template: '#Item',
-      props: ['nav_item']
+      props: ['nav_item'],
+      methods:{
+            change(obj : any){
+                  let svg_in= '<svg xmlns="http://www.w3.org/2000/svg" width="{wid}" height="{hei}" fill="currentColor" class="{#class}" viewBox="0 0 16 16">'+
+                        '{#path}'+
+                  '</svg>';
+                  svg_in = svg_in.replace('{wid}', obj.width).replace('{hei}', obj.height)
+                  .replace('{#class}', obj.class).replace('{#path}', obj.path);
+                  return svg_in;
+            }   
+      }
 }
 </script>
 
 <template :item="nav_item" id="Item">
-      <div class="col-12 navbar_itens">
-            <span v-html="nav_item.icon"></span>
+      <button class="btn col-12 navbar_itens">
+            <span v-html="change(nav_item.icon)"></span>
             {{ nav_item.abrev }}
-      </div>
+      </button>
 </template>
 
 <style scoped>
 .navbar_itens{
-      /* color: --bs-blue;
-      font-size: 16px; */
+      margin: 0;
+      padding-left: 24px;
+      font-size: 14px;
+      color: var(--bs-gray-500);
 }
 </style>
