80884397a54f5fc00e63a84fea9aebad4696bb42
Matheus dos Reis Casarim <matheuscasarim22@hotmail.com>
2024-10-22 22:37:55 -0300

    Adição da navbar como um componente

---
+++ src/components/navbars/NavbarLateral.vue
@@ -0,0 +1,168 @@
+<script lang="ts">
+import router from '@/router';
+
+export default{
+      template: '#Nav_lat',
+      methods:{
+            changePage(rota: string){
+                  router.push('/'+rota);
+            }
+      },
+      props:{
+            // eslint-disable-next-line
+            item_ativo:'',
+      }
+}
+</script>
+<!-- Bottons Rotas arrumar para ficar igual a deles -->
+<template id="Nav_lat">
+      <div class="col-12">
+            <ul class="list-group nav_lateral">
+                  <!-- Dashboard -->
+                  <li :class="['navbar_itens', {'item_nav_act' :item_ativo === 'dashboard'}]">
+                        <button  class="btn item_nav col-12" @click="changePage('dashboard')">
+                              <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-speedometer" viewBox="0 0 16 16">
+                                          <path d="M8 2a.5.5 0 0 1 .5.5V4a.5.5 0 0 1-1 0V2.5A.5.5 0 0 1 8 2M3.732 3.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707M2 8a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8m9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5m.754-4.246a.39.39 0 0 0-.527-.02L7.547 7.31A.91.91 0 1 0 8.85 8.569l3.434-4.297a.39.39 0 0 0-.029-.518z"/>
+                                          <path fill-rule="evenodd" d="M6.664 15.889A8 8 0 1 1 9.336.11a8 8 0 0 1-2.672 15.78zm-4.665-4.283A11.95 11.95 0 0 1 8 10c2.186 0 4.236.585 6.001 1.606a7 7 0 1 0-12.002 0"/>
+                                    </svg>
+                                    Dashboard
+                              </span>
+                        </button>
+                  </li>
+                  <!-- Empresas -->
+                  <li :class="['navbar_itens', {'item_nav_act' :item_ativo === 'empresas'}]">
+                        <button class="btn item_nav col-12" @click="changePage('empresas')">
+                              <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shop" viewBox="0 0 16 16">
+                                          <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.37 2.37 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0M1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5M4 15h3v-5H4zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1zm3 0h-2v3h2z"/>
+                                    </svg>
+                                    Empresas
+                              </span>
+                        </button>
+                  </li>
+                  <!-- Canais -->
+                  <li :class="['navbar_itens', {'item_nav_act' :item_ativo === 'canais'}]">
+                        <button class="btn item_nav col-12" @click="changePage('canais')">
+                              <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid-fill" viewBox="0 0 16 16">
+                                          <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5z"/>
+                                    </svg>
+                                    Canais
+                              </span>
+                        </button>
+                  </li>
+                  <!-- Ambiente -->
+                  <li :class="['navbar_itens', {'item_nav_act' :item_ativo === 'ambiente'}]">
+                        <button class="btn item_nav col-12" @click="changePage('ambiente')">
+                              <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-fill" viewBox="0 0 16 16">
+                                          <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383"/>
+                                    </svg>
+                                    Ambiente
+                              </span>
+                        </button>
+                  </li>
+                  <!-- Integrações -->
+                  <li class="navbar_itens">
+                        <button class="btn item_nav col-12">
+                              <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
+                                          <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9"/>
+                                          <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"/>
+                                    </svg>
+                                    Integrações
+                              </span>
+                        </button>
+                        <ul class="list-group">
+                              <li :class="['navbar_itens', {'item_nav_act' :item_ativo === 'mark_ecom'}]">
+                              <button class="btn item_nav col-12" @click="changePage('marketplaceecommerce')">
+                                    <span>
+                                          Marketplace/Ecommerce
+                                    </span>
+                              </button>
+                              </li>
+                              <li :class="['navbar_itens', {'item_nav_act' :item_ativo === 'map_pro'}]">
+                                    <button class="btn item_nav col-12" @click="changePage('mapeamentoprodutos')">
+                                          <span>
+                                                Mapeamento de Produtos
+                                          </span>
+                                    </button>
+                              </li>
+                        </ul>
+                  </li>
+                  <!-- Usuário -->
+                  <li :class="['navbar_itens', {'item_nav_act' :item_ativo === 'usuario'}]">
+                        <button class="btn item_nav col-12" @click="changePage('usuario')">
+                              <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                          <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                                    </svg>
+                                    Usuário
+                              </span>
+                        </button>
+                  </li>
+                  <!-- LogATT -->
+                  <li :class="['navbar_itens', {'item_nav_act' :item_ativo === 'log_att'}]">
+                        <button class="btn item_nav col-12" @click="changePage('atualizacao')">
+                              <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-journal-text" viewBox="0 0 16 16">
+                                          <path d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5"/>
+                                          <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2"/>
+                                          <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z"/>
+                                    </svg>
+                                    Logs das Atualizações
+                              </span>
+                        </button>
+                  </li>
+                  <!-- LogReq -->
+                  <li :class="['navbar_itens', {'item_nav_act' :item_ativo === 'log_req'}]">
+                        <button class="btn item_nav col-12" @click="changePage('requisicao')">
+                              <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-list" viewBox="0 0 16 16">
+                                          <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
+                                          <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8m0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0M4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0"/>
+                                    </svg>
+                                    Logs das Requisições
+                              </span>
+                        </button>
+                  </li>
+            </ul>
+      </div>
+</template>
+
+<style scoped>
+.nav_lateral{
+      background-color: var(--bs-primary-text-emphasis);
+      padding: 0;
+}
+
+.item_nav{
+      text-align: left;
+      font-size: 14px;
+      color: var(--bs-gray-500);
+      padding-left: 24px;
+      text-decoration: none;
+}
+
+.item_nav:active{
+      background-color: var(--bs-cyan);
+      color: var(--bs-gray-100);
+}
+
+.item_nav:hover{
+      color: var(--bs-gray-100);
+}
+
+
+.item_nav_act{
+      background-color: var(--bs-cyan);
+}
+.item_nav_act > .item_nav{
+      color: var(--bs-gray-100);
+}
+
+.navbar_itens{
+      margin: 0;
+}
+</style>
