860205d3d48974f39edfb6f8ae88b2ca77577d1f
Matheus dos Reis Casarim <matheuscasarim22@hotmail.com>
2024-11-09 14:43:35 -0300

    Refatoração das regras impossibilitando ela da comunicação externa

--- src/services/regras_negocio/regras_empresa.ts
+++ src/services/regras_negocio/regras_empresa.ts
@@ -9,7 +9,7 @@ class DadoEmpresa {
 }
 
 class Empresa {
-      async _add(path : string, dado : DadoEmpresa, erros_msg : Array<string>){
+      async _add(dado : DadoEmpresa, erros_msg : Array<string>){
             if(dado.descricao == ''){
                   erros_msg.push('razao');
             }
@@ -24,16 +24,10 @@ class Empresa {
             }
             if (erros_msg.length != 0) {
                   console.warn("ERRO: " , erros_msg);
-            }else{
-                  try{
-                        await Promise.resolve(fetch_.postDado(path,dado));
-                  }catch(error){
-                        erros_msg.push('400');
-                  }
             }
       }
 
-      async _edit(path : string, old_dado : DadoEmpresa, new_dado : DadoEmpresa, id : string, erros_msg : Array<string>){
+      async _edit(old_dado : DadoEmpresa, new_dado : DadoEmpresa, erros_msg : Array<string>){
             if(new_dado.descricao == ''){
                   erros_msg.push('razao');
             }
@@ -52,12 +46,6 @@ class Empresa {
             
             if (erros_msg.length != 0) {
                   console.warn("ERRO: " , erros_msg);
-            }else{
-                  try{
-                        await Promise.resolve(fetch_.putDado(path, id, new_dado));
-                  }catch(error){
-                        erros_msg.push('400');
-                  }
             }
       }
 }
