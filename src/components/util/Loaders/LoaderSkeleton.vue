67b9e289846b81944adddec37a7cb3cde6b2c4fb
Matheus dos Reis Casarim <matheuscasarim22@hotmail.com>
2025-01-10 14:07:01 -0300

    Fix (tamanho): Arrumado o tamanho do loader select

--- src/components/util/Loaders/LoaderSkeleton.vue
+++ src/components/util/Loaders/LoaderSkeleton.vue
@@ -39,6 +39,7 @@ export default defineComponent({
       padding: 0.5rem;
       border-radius: 0.25rem;
 }
+
 .custom-select{
       background: #fff url(data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 4 5'%3E%3Cpath fill='%23343a40' d='M2 0L0 2h4zm0 5L0 3h4z'/%3E%3C/svg%3E) no-repeat right .75rem center;
       background-size: 8px 10px;
@@ -51,6 +52,6 @@ export default defineComponent({
       border: 1px solid #ced4da;
       border-radius: .25rem;
       display: inline-block;
-      
+      height: auto;
 }
 </style>
