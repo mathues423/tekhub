ed0ee23d08bb6b81d325dfb0a819ab7444bac712
Matheus dos Reis Casarim <matheuscasarim22@hotmail.com>
2025-01-10 14:06:10 -0300

    Feat: Adição do componente de erro

---
+++ src/components/mensagem/ErroResponseComponent.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+      import { defineComponent } from 'vue';
+      import { APPCONFIG } from '../constants/Config';
+      export default defineComponent({
+            props:{
+                  error_msg:{
+                        type: Object,
+                        required: true
+                  }
+            },
+            methods:{
+                  backHome(){
+                        if (APPCONFIG.authToken != '') {
+                              APPCONFIG.authToken = ``
+                        }
+                        if (localStorage.getItem('TOKEN')) {
+                              localStorage.removeItem('TOKEN')
+                        }
+                        this.$router.push('/')
+                  }
+            },
+            emits:['voltar']
+      })
+</script>
+
+<template>
+      <div class="row">
+            <div class="content" style="text-align: center; margin-top: auto; margin-bottom: auto;" v-if="error_msg.errors">
+                  <div>
+                        {{ error_msg.errors }}
+                  </div>
+                  <div>
+                        <button class="btn btn-info" @click="$emit('voltar')">Voltar</button>
+                  </div>
+            </div>
+            <div class="content" style="text-align: center; margin-top: auto; margin-bottom: auto;" v-else>
+                  Sua Requisição feita as: ({{ error_msg.timestamp }}) para a rotado servidor "...{{ error_msg.path }}" <br>
+                  teve o erro [mensagem: "{{ error_msg.message }}", status: "{{ error_msg.status }} {{ error_msg.error }}"].
+            </div>
+            
+      </div>
+</template>
+
+<style scoped>
+</style>
